8f12d871add09af428b8265ccff271ba61858740
komalgb <komal.kgbb@gmail.com>
2023-12-13 10:14:09 +0530

    About Us Page Layout Complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
-var sliderManin= document.querySelector(".slider-main");
+var sliderMain= document.querySelector(".slider-main");
 let item=document.getElementsByClassName("item");
 function next()
 {
-    sliderManin.append(item[0]);
+    sliderMain.append(item[0]);
 }
 function prev()
 {
-    sliderManin.prepend(item[item.length-1]);
+    sliderMain.prepend(item[item.length-1]);
 }
 setInterval(next,5000)
